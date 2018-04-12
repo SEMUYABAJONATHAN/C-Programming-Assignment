557c5ee6e9352e6d63c7d7c5bbc602a376e359d1
SEMUYABA JONATHAN <38241921+SEMUYABAJONATHAN@users.noreply.github.com>
2018-04-12 00:40:21 -0700

    Add files via upload

---
+++ QN 1.c
@@ -0,0 +1,22 @@
+// SEMUYABA JONATHAN
+//17/U/1074
+//217000102
+//BSTE
+
+//QUESTION 0NE
+
+
+#include <stdio.h>
+
+int x = 1;
+
+main ()
+{
+    if(x == 1)
+        printf("x equals 1");
+
+    else
+        printf("x does not equal 1");
+
+        return 0;
+}
