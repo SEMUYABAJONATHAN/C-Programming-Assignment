7a7b7a9c2f374bcc4e48182998c09da0fca7036c
SEMUYABA JONATHAN <38241921+SEMUYABAJONATHAN@users.noreply.github.com>
2018-04-12 00:54:04 -0700

    Add files via upload

---
+++ QUESTION THREE.c
@@ -0,0 +1,34 @@
+
+//QUESTION THREE (a)
+
+ long values[50];
+
+//QUESTION THREE(b)
+
+ long values[49] = 123.456
+
+ //QUESTION THREE (c)
+
+ /* The values of x is 0, 1, 2, ...,99*/
+
+ // QUESTION THREE (d)
+
+ /* The values of ctr are {2, 5, 8} */
+
+ //QUESTION THREE (e)
+
+ #include <stdio.h>
+
+ main ()
+ {
+     int count = 1;
+
+     while(count <= 100)
+     {
+         printf("%d\n", count);
+         count += 3;
+     }
+     return 0;
+ }
+
+
