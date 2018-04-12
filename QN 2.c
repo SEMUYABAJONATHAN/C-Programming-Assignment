dbad92c446ff01577c05fd3841aff936e48f285c
SEMUYABA JONATHAN <38241921+SEMUYABAJONATHAN@users.noreply.github.com>
2018-04-12 00:52:04 -0700

    Add files via upload

---
+++ QN 2.c
@@ -0,0 +1,33 @@
+
+//QUESTION TWO (a)
+
+//(do_it.h)
+
+#ifndef DO_IT_H
+#define DO_IT_H
+
+float do_it(char, char, char)
+
+#endif // DO_IT_H
+
+//QUESTION TWO(b)
+
+//(print_a_number.h)
+
+#ifndef PRINT_A_NUMBER_H
+#define PRINT_A_NUMBER_H
+
+void print_a_number(int)
+
+
+#endif // PRINT_A_NUMBER_H
+
+
+//QUESTION TWO (C)
+
+/*Since print_msg() arguments have been declared void,
+that means the main function should not call it with a string argument  ie;
+print_msg("This is a message to print") but its supposed to be print_msg().
+and it must return nothing hence the "return 0" in the body of the function must be deleted
+*/
+
